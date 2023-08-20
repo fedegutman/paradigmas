db690f78d3f2c47aff83a0cec355a232799ae5ce
Federico Gutman <fgutman@udesa.edu.ar>
2023-08-20 15:38:19 -0300

    cambios

--- tp1/city.hs
+++ tp1/city.hs
@@ -1,4 +1,4 @@
-module City (City, newC, nameC, distanceC) 
+module City (City, newC, nameC, distanceC)
     where
 
 import Point
@@ -6,10 +6,10 @@ import Point
 data City = Cit String Point deriving (Eq, Show)
 
 newC :: String -> Point -> City
-newC name coordinates = Cit name coordinates
+newC = Cit
+-- newC name coordinates = Cit name coordinates
 
 nameC :: City -> String
-nameC (Cit name _) = name
 
 distanceC :: City -> City -> Float
 distanceC (Cit _ coordinates1) (Cit _ coordinates2) = difP coordinates1 coordinates2
--- tp1/point.hs
+++ tp1/point.hs
@@ -4,7 +4,9 @@ module Point (Point, newP, difP)
 data Point = Poi Int Int deriving (Eq, Show)
 
 newP :: Int -> Int -> Point
-newP x y = Poi x y
+newP = Poi
 
-difP :: Point -> Point -> Float  -- distancia absoluta # preguntar que hacemos con estos comentarios
+-- newP x y = Poi x y
+
+difP :: Point -> Point -> Float
 difP (Poi x1 y1) (Poi x2 y2) = sqrt $ fromIntegral ((x2 - x1)^2 + (y2 - y1)^2)
