bb2968bb8eb9ea09ed0a20b9dbf3f8f724c30d9c
mateobramer <bramermarkovicm@udesa.edu.ar>
2023-08-23 12:04:21 -0300

    cambio

--- tp1/tunel.hs
+++ tp1/tunel.hs
@@ -12,7 +12,6 @@ newT |
      | otherwise = error "No hay capacidad para más tuneles."
 
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
-connectsT cityA cityB (Tun [Lin city1 city2 _]) = ((cityA == city1) && (cityB == city2)) || ((cityB == city1) && (cityA == city2))
 -- diferencia entre esto y linksl
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
