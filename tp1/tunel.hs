e3feb070e2f824401da2c969d10c2041555fecea
fedegutman <fedegutman@udesa.edu.ar>
2023-08-23 10:50:32 -0300

    cambio

--- tp1/tunel.hs
+++ tp1/tunel.hs
@@ -8,7 +8,8 @@ import Quality
 data Tunel = Tun [Link] deriving (Eq, Show)
 
 newT :: [Link] -> Tunel
-newT = Tun
+newT |
+     | otherwise = error "No hay capacidad para más tuneles."
 
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
 connectsT cityA cityB (Tun [Lin city1 city2 _]) = ((cityA == city1) && (cityB == city2)) || ((cityB == city1) && (cityA == city2))
