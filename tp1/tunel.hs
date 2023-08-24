14b75b7c6ccdf654123540eabeb4f37f4679b1f0
mateobramer <bramermarkovicm@udesa.edu.ar>
2023-08-24 11:02:22 -0300

    tunel

--- tp1/tunel.hs
+++ tp1/tunel.hs
@@ -7,14 +7,18 @@ import Quality
 
 data Tunel = Tun [Link] deriving (Eq, Show)
 
+allLinksValid :: [Link] -> Bool
+allLinksValid links = all (\link -> capacityL link > 0) links
+
 newT :: [Link] -> Tunel
-newT links |  
+newT links | allLinksValid links = Tun links
            | otherwise = error "No hay capacidad para más tuneles."
 
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
 -- diferencia entre esto y linksl
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
+usesT link (Tun links) = link `elem` links 
 
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
-delayT (Tun [Lin _ _ (Qua _ _ delay)]) = delay
+delayT (Tun links) = sum (map delayL links)
