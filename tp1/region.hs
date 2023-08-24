46cce2e15c7897c606beaa00960a55f5828ea829
mateobramer <bramermarkovicm@udesa.edu.ar>
2023-08-24 14:06:33 -0300

    region

--- tp1/region.hs
+++ tp1/region.hs
@@ -16,9 +16,14 @@ foundR (Reg cit links tunels) city = Reg ((city : cit) links tunels)
 
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
 linkR (Reg cit links tunels) cityA cityB calidad = Reg (cit (newL cityA cityB calidad : links) tunels)
-  
+
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
+tunelR Reg cit links tunels [cities] = Reg cit links  --incompleto
+
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
+connectedR (Reg cit link tunnels) cityA cityB = connectsT cityA CityB tunnels  
+
 linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
+linkedR (Reg cit link tunnels) cityA cityB = linksL
 delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
 availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
