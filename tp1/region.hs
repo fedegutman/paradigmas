9716a652436549142467a9470343f0349688f2cc
mateobramer <bramermarkovicm@udesa.edu.ar>
2023-08-24 13:03:59 -0300

    region

--- tp1/region.hs
+++ tp1/region.hs
@@ -12,9 +12,11 @@ newR :: Region
 newR = Reg [] [] []
 
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
-foundR (Reg cit _ _) city = city : cit
+foundR (Reg cit links tunels) city = Reg ((city : cit) links tunels)
 
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
+linkR (Reg cit links tunels) cityA cityB calidad = Reg (cit (newL cityA cityB calidad : links) tunels)
+  
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
 linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
