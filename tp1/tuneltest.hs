6de6e48504c1184d772b89287d7e04dbcbf18253
Federico Gutman <fgutman@udesa.edu.ar>
2023-08-25 16:47:22 -0300

    cambios tuneles

--- tp1/tuneltest.hs
+++ tp1/tuneltest.hs
@@ -7,14 +7,15 @@ import Quality
 
 data Tunel = Tun [Link] deriving (Eq, Show)
 
-
 newT :: [Link] -> Tunel
-newT = Tun 
+newT = Tun
 
 connectsT :: City -> City -> Tunel -> Bool
+connectsT cityA cityB (Tun links) = (head links) (last links)
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
-usesT link (Tun links) = link `elem` links 
+usesT link (Tun links) = link `elem` links
 
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
-delayT (Tun links) = sum (map delayL links)
+-- delayT (Tun links) = sum (map delayL links)
+delayT (Tun links) = sum [ delayL link | link <- links]
