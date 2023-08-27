9082dd44fa7a4fb04c30f42ea42ed64abe468b61
Federico Gutman <fgutman@udesa.edu.ar>
2023-08-27 16:12:13 -0300

    cambios region

--- tp1/region.hs
+++ tp1/region.hs
@@ -1,4 +1,3 @@
-
 module Region (Region, newR, foundR, linkR, {-tunelR,-} connectedR, linkedR, delayR, availableCapacityForR)
    where
 
@@ -13,14 +12,14 @@ newR :: Region
 newR = Reg [] [] []
 
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
-foundR (Reg cities links tunels) city | not (locationTaken cities city) = Reg (city : cities) links tunels
+foundR (Reg cities links tuneles) city | not (locationTaken cities city) = Reg (city : cities) links tuneles
                                       | otherwise = error "Ya hay una ciudad en esas coordenadas."
 
 locationTaken :: [City] -> City -> Bool
 locationTaken cities newcity = null ([city | city <- cities, distanceC city newcity == 0])
 
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
-linkR (Reg cit links tunels) cityA cityB quality | (cityA `elem` cit) && (cityB `elem` cit) = Reg cit (newL cityA cityB quality : links) tunels
+linkR (Reg cit links tuneles) cityA cityB quality | (cityA `elem` cit) && (cityB `elem` cit) = Reg cit (newL cityA cityB quality : links) tuneles
                                                  | existingLink links cityA cityB = error "Ya existe un enlace entre estas ciudades."
                                                  | otherwise = error "Al menos una de las dos ciudades no está en la región."
 
@@ -36,9 +35,9 @@ consecutiveCities (x:y:xs) = [x, y] : consecutiveCities (y:xs)
 -- link2ciudadeslist [cityA, cityB] = newL cityA cityB _
 {-
 tunelR :: Region -> [City] -> Region -- genera una comunicación entre dos ciudades distintas de la región
-tunelR (Reg cit links tunels) cities | (cityA `elem` cit) && (cityB `elem` cit)
+tunelR (Reg cit links tuneles) cities | (cityA `elem` cit) && (cityB `elem` cit)
                                      | 
-                                     | Reg cit links newT (map link2ciudadeslist (consecutiveCities [cities])):tunels
+                                     | Reg cit links newT (map link2ciudadeslist (consecutiveCities [cities])):tuneles
 -}
 
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
@@ -47,17 +46,15 @@ connectedR (Reg _ _ tuneles) cityA cityB = not (null [tunel | tunel <- tuneles, 
 linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
 linkedR (Reg _ links _) cityA cityB = not (null [link | link <- links, linksL cityA cityB link])
 
-encontarTunel :: Region -> City -> City -> Tunel
-encontarTunel reg [_] [link] [tunel] cityA cityB = head (filter (connectsT cityA cityB) tunel)
-
 delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
-delayR reg _ _ tunnels cityA cityB = delayT (encontarTunel Reg _ _ tunnels cityA cityB)
+delayR (Reg cities links tuneles) cityA cityB | connectedR (Reg cities links tuneles) cityA cityB = sum [delayT tunel | tunel <- tuneles, connectsT cityA cityB tunel]
+                                              | otherwise = error "Las ciudades no están conectadas por un tunel."
 
-encontarCapacitymasBaja :: Tunel -> Int      --NO SIRVE PARA NADA 
-encontarCapacitymasBaja tunel links = minimum (map capacityL links)
-
-encontarLink :: Region -> City -> City -> Link
-encontarLink reg _ link _ cityA cityB = head (filter (linksL cityA cityB) link)
+--CHEQUEAR
 
 availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
-availableCapacityForR reg _ link _ cityA cityB = capacityL (head (filter (linksL cityA cityB) link)) - 1
+availableCapacityForR (Reg cities links tuneles) cityA cityB | not (linkedR (Reg cities links tuneles) cityA cityB) = error "Las ciudades no están conectadas por un enlace."
+                                                             | otherwise = capacityL (head (filter (linksL cityA cityB) links))
+
+--CHEQUEAR
+                  
