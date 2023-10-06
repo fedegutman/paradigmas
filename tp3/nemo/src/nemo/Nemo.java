b5049f092d8b3b6b03287791d2db7406024fed80
mateobramer <bramermarkovicm@udesa.edu.ar>
2023-10-06 19:22:48 -0300

    algo

--- tp3/nemo/src/nemo/Nemo.java
+++ tp3/nemo/src/nemo/Nemo.java
@@ -9,12 +9,14 @@ public class Nemo {
 	public int xcoordinate;
 	public int ycoordinate;
 	public String direction;
+	public boolean capsule ;
 
 	public Nemo() {
 		depth = 0;
 		xcoordinate = 0;
 		ycoordinate = 0;
 		direction = "North";
+		capsule = true ;
 
 	};
 
@@ -22,8 +24,13 @@ public class Nemo {
 		if (direction == "") {
 			return this;
 		}
+		else {
+	        direction.chars().forEach(step -> {
+	            char stepChar = (char) step;
+	            takeStep(stepChar);
+	        });
+	    }
 		return null;
-
 	}
 	
 	public Nemo takeStep(char step) {
@@ -39,9 +46,82 @@ public class Nemo {
 				this.depth += 1;
 				return this;
 		}
+			else if (step == 'l' && direction == "North") {
+				this.direction = "West";
+				return this;
+			}
+			else if (step == 'r'&& direction == "North") {
+				this.direction = "East";
+				return this;
+
+			}
+			else if (step == 'r' && direction == "East") {
+				this.direction = "South";
+				return this;
+
+			}
 			
-		return null;
+			else if (step == 'r'&& direction == "South") {
+				this.direction = "West";
+				return this;
 
-	}
+			}
+			
+			else if (step == 'r'&& direction == "West") {
+				this.direction = "North";
+				return this;
 
-};
+			}
+			
+			else if (step == 'l'&& direction == "West") {
+				this.direction = "South";
+				return this;
+
+			}
+			
+			
+			else if (step == 'l'&& direction == "South") {
+				this.direction = "East";
+				return this;
+
+			}
+			
+			else if (step == 'l'&& direction == "East") {
+				this.direction = "North";
+				return this;
+
+			}
+			
+		
+			else if (step == 'f'&& direction == "North") {
+				this.ycoordinate +=1 ;
+				return this;
+
+			}
+			else if (step == 'f' && direction == "East") {
+				this.xcoordinate +=1;
+				return this;
+
+			}
+			
+			else if (step == 'f'&& direction == "South") {
+				this.ycoordinate -=1;
+				return this;
+
+			}
+			
+			else if (step == 'f'&& direction == "West") {
+				this.xcoordinate -=1;
+				return this;
+
+			}
+			
+			else if (step == 'm' && depth < -1 ) {
+				this.capsule = false ;
+				return this ;
+			}
+			return this;
+			
+			
+
+};}
